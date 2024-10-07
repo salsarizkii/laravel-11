61dfd6ab6595edf1bdf303782ea2cc7132c0b5ad
salsarizkii <salsarizki.srs@gmail.com>
2024-10-07 23:27:58 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>ini contact salsa</h1>
+    <a href="https://www.instagram.com/salsaa.rs">ini instagram</a>
+    <a href="https://wa.me/085743863245">kalo ini wasap</a>
+</body>
+</html>
