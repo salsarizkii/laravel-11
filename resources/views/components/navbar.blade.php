756b346a66f8f8b2f63fe0474c72bd93def3bc2b
salsarizkii <salsarizki.srs@gmail.com>
2024-10-13 17:53:50 +0700

    navbar fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,15 +9,15 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               
-              <x-navlink href="/" >/</x-navlink>
+              {{-- <x-navlink href="/" >Home</x-navlink>
               <x-navlink href="/blog" >blog</x-navlink>
               <x-navlink href="/about" >about</x-navlink>
-              <x-navlink href="/contact" >contact</x-navlink>
+              <x-navlink href="/contact" >contact</x-navlink> --}}
 
-              {{-- <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
+              <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
               <x-navlink href="/blog" :active="request()->is('blog')">Blog</x-navlink>
               <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
-              <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink> --}}
+              <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink>
             </div>
           </div>
         </div>
